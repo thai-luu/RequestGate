8d2a24539130beb991874265c0d9e21470f47a32
thai-luu <thailuu0928@gmail.com>
2021-06-08 23:38:35 +0700

    fix db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,9 +20,12 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('role');
+            $table->unsignedBigInteger('department_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
+           
         });
+       
     }
 
     /**
